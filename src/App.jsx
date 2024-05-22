be55a0461092b7bf9c1702637e504438c1c98b75
John-Dev <tst.1a2a3@gmail.com>
2024-05-22 23:36:14 +0200

    Add Link and little styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import About from "./About";
 import Home from "./Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./style.css"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
