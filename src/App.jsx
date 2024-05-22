c9cf8862f2e2a19915b5f4c917d4533114fc9c7c
John-Dev <tst.1a2a3@gmail.com>
2024-05-22 23:28:39 +0200

    Add basic routing implmentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import About from "./About";
+import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="main-part">
-      <h1>Hello react</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
