211c7df8f01d9d8d63eb3b76cf3c2096f4782ce8
John-Dev <tst.1a2a3@gmail.com>
2024-05-24 20:43:54 +0200

    Add link to google fonts in index.html and new route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import About from "./pages/About";
 import Home from "./pages/Home";
-import "./styles/app.css";
+import Cars from "./pages/Cars";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
       <header>
-        <Link to="/">Rent a car</Link>
-        <nav>
-          <Link to="/about">About</Link>
+        <div className="logo">
+          <Link to="/">Rent a car</Link>
+        </div>
+        <nav> 
+          <Link to="/about" className="nav-link">About</Link>
+          <Link to="/cars" className="nav-link">Cars</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/cars" element={<Cars />} />
       </Routes>
     </BrowserRouter>
   );
