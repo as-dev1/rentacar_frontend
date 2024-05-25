7ade8221da3b1b2800a8a67c7ecde418970f04ce
John-Dev <tst.1a2a3@gmail.com>
2024-05-25 12:46:43 +0200

    add car details page with responsive css

--- src/pages/CarDetail.jsx
+++ src/pages/CarDetail.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const CarDetail = () => {
-  return (
-    <div>CarDetail</div>
-  )
-}
+  const [car, setCar] = useState(null);
+  const { id } = useParams();
 
-export default CarDetail
+  useEffect(() => {
+    loadCar();
+  }, [id]);
+
+  const loadCar = async () => {
+    const result = await axios.get(`http://localhost:8080/api/car/${id}`);
+    setCar(result.data);
+  };
+
+  return (
+    <div className="car-detail-wrapper">
+      {car ? (
+        <div className="car-detail">
+          <div className="car-image">
+            <img src={car.img_path} alt={`${car.brand} ${car.model}`} />
+          </div>
+          <div className="car-info">
+            <h1>{car.brand} {car.model}</h1>
+            <ul>
+              <li><b>Year:</b> {car.year}</li>
+              <li><b>Horsepower:</b> {car.hp} hp</li>
+              <li><b>Seats:</b> {car.number_of_seats}</li>
+              <li><b>Price:</b> ${car.price_per_day}/day</li>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
+};
+
+export default CarDetail;
--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -17,7 +17,7 @@ const Cars = () => {
   const carElement = cars.map((car) => (
     <div key={car.id} className="car_card">
       <Link to={`/cars/${car.id}`}>
-        <img className="car_img" src={car.imgPath} alt="" />
+        <img className="car_img" src={car.img_path} alt={`${car.brand} ${car.model}`} />
         <div className="car_stats">
           <h3>{car.brand}</h3>
           <h4>{car.model}</h4>
