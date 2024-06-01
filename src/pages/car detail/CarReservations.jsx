4787a66b0cecd466ffa58493f4f7100419e46a71
John-Dev <tst.1a2a3@gmail.com>
2024-06-01 21:30:39 +0200

    add reservation logic and display them

--- src/pages/car detail/CarReservations.jsx
+++ src/pages/car detail/CarReservations.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import { useOutletContext } from 'react-router-dom'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faCalendar } from "@fortawesome/free-solid-svg-icons";
 
 const CarReservations = () => {
-  const { car } = useOutletContext();
-  
-  return (
-    <div>Car Reservations</div>
-  )
-}
+  const { id } = useParams();
+  const [reservations, setReservations] = useState([]);
 
-export default CarReservations
+  useEffect(() => {
+    loadReservations();
+  }, []);
+
+  const loadReservations = async () => {
+    const result = await axios.get(`http://localhost:8080/api/reservation/car/${id}`);
+    setReservations(result.data);
+  };
+
+  const reservationElement = reservations.map((reservation) => (
+    <div key={reservation.id} className="container border-bottom py-2">
+      <div className="d-flex">
+        <FontAwesomeIcon icon={faUser} className="fs-4" />
+        <p className="text-primary fw-bold px-2">
+          {reservation.user.first_name} {reservation.user.last_name}
+        </p>
+      </div>
+      <p>
+        <FontAwesomeIcon icon={faCalendar} />
+        <strong className="px-2">From date: </strong>
+        <span className="fst-italic">{new Date(reservation.start_at).toLocaleString('sr-RS')}</span>
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faCalendar} />
+        <strong className="px-2">To date: </strong>
+        <span className="fst-italic">{new Date(reservation.end_at).toLocaleString('sr-RS')}</span>
+      </p>
+    </div>
+  ));
+
+  return (
+    <div className="px-2">{reservationElement}</div>
+  );
+};
+
+export default CarReservations;
