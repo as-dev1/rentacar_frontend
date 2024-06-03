1edaef7046300a785083d347bef88b7ba92476d0
John-Dev <tst.1a2a3@gmail.com>
2024-06-03 17:25:52 +0200

    change header from custom css to bootstrap navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const toggleNavbar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  const active = {
+    textDecoration: "underline",
+    fontWeight: "bold"
+  }
+
   return (
-    <header>
-      <div className="logo">
-        <Link to=".">Rent a car</Link>
+    <header className="navbar navbar-expand-sm border-bottom shadow-sm">
+      <div className="container-fluid">
+        <Link className="navbar-brand fw-bold fs-4 text-light" to=".">
+          Rent a car
+        </Link>
+        <button className="navbar-toggler" type="button" onClick={toggleNavbar} aria-expanded={!isCollapsed} aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <nav className={`collapse navbar-collapse ${!isCollapsed ? "show" : ""}`}>
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex align-items-center">
+            <li className="nav-item">
+              <NavLink to="cars" className="nav-link fs-5 text-light" style={({ isActive }) => isActive ? active : null}>
+                Cars
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="addcar" className="nav-link fs-5 text-light" style={({ isActive }) => isActive ? active : null}>
+                Add Car
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="about" className="nav-link fs-5 text-light" style={({ isActive }) => isActive ? active : null}>
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <NavLink to="cars" className={({ isActive }) => (isActive ? "active-page" : null)}>
-          Cars
-        </NavLink>
-        <NavLink to="addcar" className={({ isActive }) => (isActive ? "active-page" : null)}>
-          Add Car
-        </NavLink>
-        <NavLink to="about" className={({ isActive }) => (isActive ? "active-page" : null)}>
-          About
-        </NavLink>
-      </nav>
     </header>
   );
 };
