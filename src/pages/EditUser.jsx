cf72fa87f649b0147c2bbc8de2cf712ab8ad2bc4
John-Dev <tst.1a2a3@gmail.com>
2024-06-04 16:13:41 +0200

    add edit user functionality

--- src/pages/EditUser.jsx
+++ src/pages/EditUser.jsx
@@ -1,8 +1,120 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const EditUser = () => {
+    let navigate = useNavigate();
+
+    const { id } = useParams();
+  
+    const [user, setUser] = useState({
+      first_name: "",
+      last_name: "",
+      username: "",
+      phone_number: "",
+      email: "",
+    });
+  
+    const { first_name, last_name, username, phone_number, email } = user;
+  
+    const onInputChange = (e) => {
+      setUser({ ...user, [e.target.name]: e.target.value });
+    };
+  
+    useEffect(() => {
+      loadUser();
+    }, []);
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await axios.put(`http://localhost:8080/api/user/${id}`, user);
+        navigate("/users");
+      };
+    
+      const loadUser = async () => {
+        const result = await axios.get(`http://localhost:8080/api/user/${id}`);
+        setUser(result.data);
+      };
   return (
-    <div>EditUser</div>
+    <div className="container">
+      <div className="col-md-6 offset-md-3 border rounded p-4 my-4 shadow text-center">
+        <h2 className="text-center m-4">Edit User</h2>
+
+        <form onSubmit={(e) => onSubmit(e)}>
+          <div className="mb-3">
+            <label htmlFor="First name" className="form-label">
+              First name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter your first name"
+              name="first_name"
+              value={first_name}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Last name" className="form-label">
+              Last name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter your last name"
+              name="last_name"
+              value={last_name}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Username" className="form-label">
+              Username
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter your username"
+              name="username"
+              value={username}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Phone_number" className="form-label">
+              Phone number
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter your phone number"
+              name="phone_number"
+              value={phone_number}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Email" className="form-label">
+              Email
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter your E-mail"
+              name="email"
+              value={email}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <button type="submit" className="btn btn-outline-primary">
+            Submit
+          </button>
+          <Link className="btn btn-outline-danger mx-2" to="/users">
+            Cancel
+          </Link>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -20,45 +20,49 @@ const Users = () => {
   };
 
   return (
-    <div className="container overflow-scroll">
+    <div className="container">
       <div className="py-4">
-        <table className="table border shadow">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Username</th>
-              <th scope="col">Email</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user, index) => (
+        <div className="table-responsive-md">
+          <table className="table table-responsive-md border shadow">
+            <thead>
               <tr>
-                <th scope="row" key={index}>
-                  {index + 1}
-                </th>
-                <td>{user.first_name} {user.last_name}</td>
-                <td>{user.username}</td>
-                <td>{user.email}</td>
-                <td>
-                  <Link
-                    className="btn  btn-sm btn-outline-primary mx-2"
-                    to={`/edituser/${user.id}`}
-                  >
-                    Edit
-                  </Link>
-                  <button
-                    className="btn btn-sm btn-danger mx-2"
-                    onClick={() => deleteUser(user.id)}
-                  >
-                    Delete
-                  </button>
-                </td>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Username</th>
+                <th scope="col">Email</th>
+                <th scope="col">Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <tr key={user.id}>
+                  <th scope="row" key={user.id}>
+                    {index + 1}
+                  </th>
+                  <td className="text-nowrap">{user.first_name} {user.last_name}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>
+                  <div className="d-flex">
+                    <Link
+                      className="btn btn-sm btn-outline-primary mx-1 my-1"
+                      to={`/edituser/${user.id}`}
+                    >
+                      Edit
+                    </Link>
+                    <button
+                      className="btn btn-sm btn-danger mx-1 my-1"
+                      onClick={() => deleteUser(user.id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
